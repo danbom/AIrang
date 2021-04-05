480cf055bd351b924a34262c6dc7a737e2f82d0e
danbom <52441697+danbom@users.noreply.github.com>
2021-04-05 18:38:26 +0900

    Add files via upload

---
+++ 웹구현/data.js
@@ -0,0 +1,32 @@
+var song_list = {
+  "01": {
+    title: "별과 고양이",
+    artist: "민은영",
+    duration: "1:50",
+    track: "Stars_and_a_Cat.mp3",
+  },
+  "02": {
+    title: "노래제목1",
+    artist: "이름1",
+    duration: "1:50",
+    track: "Stars_and_a_Cat.mp3",
+  },
+  "03": {
+    title: "노래제목2",
+    artist: "이름2",
+    duration: "1:50",
+    track: "Stars_and_a_Cat.mp3",
+  },
+  "04": {
+    title: "노래제목3",
+    artist: "이름3",
+    duration: "1:50",
+    track: "Stars_and_a_Cat.mp3",
+  },
+  "05": {
+    title: "노래제목4",
+    artist: "이름4",
+    duration: "1:50",
+    track: "Stars_and_a_Cat.mp3",
+  },
+};
